d64bbfa7cabb69c080e2f566bf68457c07f4a359
Kalyanvangipurapu <43434411+Kalyanvangipurapu@users.noreply.github.com>
2019-06-04 12:59:22 +0530

    Including string (All, Each & Get) functions

--- pmemkv.py
+++ pmemkv.py
@@ -1,3 +1,35 @@
+'''
+ * Copyright 2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+
 import pmemkv_NI
 
 class KVEngine():
@@ -23,6 +55,10 @@ class KVEngine():
     def get(self, key):
         return pmemkv_NI.get(key)
 
+    def get_string(self, key, encoding = 'utf-8'):
+        result = pmemkv_NI.get(key)
+        return None if (result == None) else result.encode(encoding)
+    
     def all(self, func):
         pmemkv_NI.all(func)
 
@@ -35,6 +71,18 @@ class KVEngine():
     def all_between(self, key1, key2, func):
         pmemkv_NI.all_between(key1, key2, func)
 
+    def all_strings(self, func, encoding = 'utf-8'):
+        pmemkv_NI.all(lambda k: func(k.encode(encoding)))
+
+    def all_strings_above(self, key, func, encoding = 'utf-8'):
+        pmemkv_NI.all_above(key, lambda k: func(k.encode(encoding)))
+
+    def all_strings_below(self, key, func, encoding = 'utf-8'):
+        pmemkv_NI.all_below(key, lambda k: func(k.encode(encoding)))
+
+    def all_strings_between(self, key1, key2, func, encoding = 'utf-8'):
+        pmemkv_NI.all_between(key1, key2, lambda k: func(k.encode(encoding)))
+        
     def count(self):
         return pmemkv_NI.count()
 
@@ -59,11 +107,23 @@ class KVEngine():
     def each_between(self, key1, key2, func):
         pmemkv_NI.each_between(key1, key2, func)
 
+    def each_string(self, func, encoding = 'utf-8'):
+        pmemkv_NI.each(lambda k, v: func(k.encode(encoding), v.encode(encoding)))
+
+    def each_string_above(self, key, func, encoding = 'utf-8'):
+        pmemkv_NI.each_above(key, lambda k, v: func(k.encode(encoding), v.encode(encoding)))
+
+    def each_string_below(self, key, func, encoding = 'utf-8'):
+        pmemkv_NI.each_below(key, lambda k, v: func(k.encode(encoding), v.encode(encoding)))
+
+    def each_string_between(self, key1, key2, func, encoding = 'utf-8'):
+        pmemkv_NI.each_between(key1, key2, lambda k, v: func(k.encode(encoding), v.encode(encoding)))
+
     def exists(self, key):
         return bool(pmemkv_NI.exists(key))
 
     def remove(self, key):
         returned = pmemkv_NI.remove(key)
         if returned < 0:
-            raise RuntimeError("Unable to put " +str(key))
+            raise RuntimeError("Unable to remove " +str(key))
         return bool(returned)
