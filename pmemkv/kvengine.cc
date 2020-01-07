3284fae55ba2e0a2395235bae402a8c9f7cd4773
Pawel Karczewski <pawel.karczewski@intel.com>
2020-01-07 13:20:07 +0100

    Introduce Buffer Protocol
    Non-copy access to variables and keys in callback functions passed to
    pmemkv_NI_get, pmemkv_NI_get_all, pmemkv_NI_get_above, pmemkv_NI_get_below,
    pmemkv_NI_get_between. Way to do it, is to create additional object type
    PmemkvValueBufferObject, avialable only in callback functions. This object
    implements Buffer Protocol. This idicate usage of memoryview inside callback
    functions.
    Function, which returns value as variable (through copy) was renamed
    from get() to get_string(). get() currently gives access to data
    throught buffer protocol.
    As usage of very similar lambda functions in callbacks implemetation was error
    prone, it was moved to named functions value_callback, key_callback and
    key_value_callback.
    
    Existing tests are adapted to all those changes and new tests for
    callbacks are added

--- pmemkv/kvengine.cc
+++ pmemkv/kvengine.cc
@@ -45,6 +45,118 @@ extern "C" {
 
 typedef struct {
 	PyObject_HEAD
+	const char *value;
+	Py_ssize_t length;
+} PmemkvValueBufferObject;
+
+static PyMemberDef PmemvValueBuffer_members[] = {
+	{"value", T_INT, offsetof(PmemkvValueBufferObject, value), 0, "Pointer to data"},
+	{"length", T_INT, offsetof(PmemkvValueBufferObject, length), 0,
+	 "Length of underlying data"},
+	{NULL}};
+
+static int PmemkvValueBufferObject_getbuffer(PyObject *obj, Py_buffer *view, int flags)
+{
+	if (view == NULL) {
+		PyErr_SetString(PyExc_ValueError, "NULL view in getbuffer");
+		return -1;
+	}
+	PmemkvValueBufferObject *self = (PmemkvValueBufferObject *)obj;
+	view->obj = obj;
+	view->buf = (void *)self->value;
+	view->len = self->length;
+	view->readonly = 1;
+	view->itemsize = 1;
+	view->format = 0; // unsigned bytes
+	view->ndim = 0;
+	view->shape = NULL;
+	view->strides = NULL;
+	view->suboffsets = NULL;
+	view->internal = NULL;
+	Py_INCREF(self);
+	return 0;
+}
+
+static PyBufferProcs PmemkvValueBuffer_as_buffer = {
+	// this definition is only compatible with Python 3.3 and above
+	(getbufferproc)PmemkvValueBufferObject_getbuffer,
+	(releasebufferproc)0, // we do not require any special release function
+};
+
+static PyObject *PmemkvValueBuffer_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+{
+	PmemkvValueBufferObject *self =
+		(PmemkvValueBufferObject *)type->tp_alloc(type, 0);
+	return (PyObject *)self;
+}
+
+static int PmemkvValueBuffer_init(PmemkvValueBufferObject *self)
+{
+	if (self != NULL) {
+		self->value = NULL;
+		self->length = 0;
+	}
+	return 0;
+}
+
+static void PmemkvValueBuffer_dealloc(PmemkvValueBufferObject *self)
+{
+	PyObject_Del(self);
+}
+
+static PyTypeObject PmemkvValueBufferType = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "pmemkv.pmemkv_NI",
+	.tp_basicsize = sizeof(PmemkvValueBufferObject),
+	.tp_itemsize = 0,
+	.tp_dealloc = (destructor)PmemkvValueBuffer_dealloc,
+	.tp_print = 0,
+	.tp_getattr = 0,
+	.tp_setattr = 0,
+	.tp_as_async = 0,
+	.tp_repr = 0,
+	.tp_as_number = 0,
+	.tp_as_sequence = 0,
+	.tp_as_mapping = 0,
+	.tp_hash = 0,
+	.tp_call = 0,
+	.tp_str = 0,
+	.tp_getattro = 0,
+	.tp_setattro = 0,
+	.tp_as_buffer = &PmemkvValueBuffer_as_buffer,
+	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+	.tp_doc = "Pmemkv value type", /* tp_doc */
+	.tp_traverse = 0,
+	.tp_clear = 0,
+	.tp_richcompare = 0,
+	.tp_weaklistoffset = 0,
+	.tp_iter = 0,
+	.tp_iternext = 0,
+	.tp_methods = 0,
+	.tp_members = PmemvValueBuffer_members,
+	.tp_getset = 0,
+	.tp_base = 0,
+	.tp_dict = NULL,
+	.tp_descr_get = 0,
+	.tp_descr_set = 0,
+	.tp_dictoffset = 0,
+	.tp_init = (initproc)PmemkvValueBuffer_init,
+	.tp_alloc = 0,
+	.tp_new = PmemkvValueBuffer_new,
+	.tp_free = 0,
+	.tp_is_gc = 0, /* For PyObject_IS_GC */
+	.tp_bases = 0,
+	.tp_mro = 0,
+	.tp_cache = 0,
+	.tp_subclasses = 0,
+	.tp_weaklist = 0,
+	.tp_del = 0,
+	.tp_version_tag = 0,
+	.tp_finalize = 0,
+};
+
+typedef struct {
+	PyObject_HEAD
 	pmemkv_db *db;
 } PmemkvObject;
 
@@ -63,11 +175,11 @@ Pmemkv_new(PyTypeObject *type, PyObject *args, PyObject *kwds) {
 	return (PyObject *) self;
 }
 
-
 static int Pmemkv_init(PmemkvObject *self)
 {
 	return 0;
 }
+
 // Turn on/off operations.
 static PyObject *
 pmemkv_NI_Start(PmemkvObject *self, PyObject* args) {
@@ -114,6 +226,71 @@ Pmemkv_dealloc(PmemkvObject *self) {
     Py_TYPE(self)->tp_free((PyObject *) self);
 }
 
+void value_callback(const char *value, size_t valuebyte, void *context)
+{
+	PmemkvValueBufferObject *entry =
+		PyObject_New(PmemkvValueBufferObject, &PmemkvValueBufferType);
+	if (entry) {
+		entry->value = value;
+		entry->length = valuebyte;
+		PyObject *args = PyTuple_New(1);
+		if (args != NULL) {
+			if (PyTuple_SetItem(args, 0, (PyObject *)entry) == 0) {
+				PyObject *res =
+					PyObject_CallObject((PyObject *)context, args);
+				Py_XDECREF(res);
+			}
+		}
+		Py_XDECREF(args);
+	} else {
+		PyErr_SetObject(PyExc_Exception,
+				PyLong_FromLong(PMEMKV_STATUS_STOPPED_BY_CB));
+	}
+}
+
+int key_callback(const char *key, size_t keybytes, const char *value, size_t valuebyte,
+		 void *context)
+{
+	value_callback(key, keybytes, context);
+	if (PyErr_Occurred() != NULL)
+		return -1;
+	return 0;
+}
+
+int key_value_callback(const char *key, size_t keybytes, const char *value,
+		       size_t valuebyte, void *context)
+{
+	PmemkvValueBufferObject *value_buffer =
+		PyObject_New(PmemkvValueBufferObject, &PmemkvValueBufferType);
+	PmemkvValueBufferObject *key_buffer =
+		PyObject_New(PmemkvValueBufferObject, &PmemkvValueBufferType);
+	Py_INCREF(value_buffer);
+	Py_INCREF(key_buffer);
+	int retval = 0;
+	if (value_buffer) {
+		value_buffer->value = value;
+		value_buffer->length = valuebyte;
+	}
+	if (key_buffer) {
+		key_buffer->value = key;
+		key_buffer->length = keybytes;
+	}
+	PyObject *args = PyTuple_New(2);
+	if (PyTuple_SetItem(args, 0, (PyObject *)key_buffer) != 0) {
+		retval = -1;
+	} else if (PyTuple_SetItem(args, 1, (PyObject *)value_buffer) != 0) {
+		retval = -1;
+	}
+	PyObject *res = PyObject_CallObject((PyObject *)context, args);
+	Py_DECREF(value_buffer);
+	Py_DECREF(key_buffer);
+	Py_DECREF(args);
+	Py_XDECREF(res);
+	if (PyErr_Occurred() != NULL)
+		retval = -1;
+	return retval;
+}
+
 // "All" Methods.
 static PyObject *
 pmemkv_NI_GetKeys(PmemkvObject *self, PyObject* args) {
@@ -121,12 +298,9 @@ pmemkv_NI_GetKeys(PmemkvObject *self, PyObject* args) {
 	if (!PyArg_ParseTuple(args, "O:set_callback", &python_callback)) {
 		return NULL;
 	}
-	auto callback = [](const char* key, size_t keybytes, const char* value, size_t valuebyte, void* context) -> int {
-		PyObject* args = Py_BuildValue("(s#)", key, keybytes);
-		PyObject_CallObject((PyObject *) context, args);
-		return 0;
-	};
-	int result = pmemkv_get_all(self->db, callback, python_callback);
+	int result = pmemkv_get_all(self->db, key_callback, python_callback);
+	if (PyErr_Occurred() != NULL)
+		return NULL;
 	if (result != PMEMKV_STATUS_OK) {
 		PyErr_SetObject(PyExc_Exception, PyLong_FromLong(result));
 		return NULL;
@@ -141,12 +315,10 @@ pmemkv_NI_GetKeysAbove(PmemkvObject *self, PyObject* args) {
 	if (!PyArg_ParseTuple(args, "s*O:set_callback", &key, &python_callback)) {
 		return NULL;
 	}
-	auto callback = [](const char* key, size_t keybytes, const char* value, size_t valuebyte, void* context) -> int {
-		PyObject* args = Py_BuildValue("(s#)", key, keybytes);
-		PyObject_CallObject((PyObject*) context, args);
-		return 0;
-	};
-	int result = pmemkv_get_above(self->db, (const char*) key.buf, key.len, callback, python_callback);
+	int result = pmemkv_get_above(self->db, (const char *)key.buf, key.len,
+				      key_callback, python_callback);
+	if (PyErr_Occurred() != NULL)
+		return NULL;
 	if (result != PMEMKV_STATUS_OK) {
 		PyErr_SetObject(PyExc_Exception, PyLong_FromLong(result));
 		return NULL;
@@ -161,12 +333,10 @@ pmemkv_NI_GetKeysBelow(PmemkvObject *self, PyObject* args) {
 	if (!PyArg_ParseTuple(args, "s*O:set_callback", &key, &python_callback)) {
 		return NULL;
 	}
-	auto callback = [](const char* key, size_t keybytes, const char* value, size_t valuebyte, void* context) -> int {
-		PyObject* args = Py_BuildValue("(s#)", key, keybytes);
-		PyObject_CallObject((PyObject *) context, args);
-		return 0;
-	};
-	int result = pmemkv_get_below(self->db, (const char*) key.buf, key.len, callback, python_callback);
+	int result = pmemkv_get_below(self->db, (const char *)key.buf, key.len,
+				      key_callback, python_callback);
+	if (PyErr_Occurred() != NULL)
+		return NULL;
 	if (result != PMEMKV_STATUS_OK) {
 		PyErr_SetObject(PyExc_Exception, PyLong_FromLong(result));
 		return NULL;
@@ -181,12 +351,11 @@ pmemkv_NI_GetKeysBetween(PmemkvObject *self, PyObject* args) {
 	if (!PyArg_ParseTuple(args, "s*s*O:set_callback", &key1, &key2, &python_callback)) {
 		return NULL;
 	}
-	auto callback = [](const char* key, size_t keybytes, const char* value, size_t valuebyte, void* context) -> int {
-		PyObject* args = Py_BuildValue("(s#)", key, keybytes);
-		PyObject_CallObject((PyObject *) context, args);
-		return 0;
-	};
-	int result = pmemkv_get_between(self->db, (const char*) key1.buf, key1.len, (const char*) key2.buf, key2.len, callback, python_callback);
+	int result = pmemkv_get_between(self->db, (const char *)key1.buf, key1.len,
+					(const char *)key2.buf, key2.len, key_callback,
+					python_callback);
+	if (PyErr_Occurred() != NULL)
+		return NULL;
 	if (result != PMEMKV_STATUS_OK) {
 		PyErr_SetObject(PyExc_Exception, PyLong_FromLong(result));
 		return NULL;
@@ -258,12 +427,9 @@ pmemkv_NI_GetAll(PmemkvObject *self, PyObject* args) {
 	if (!PyArg_ParseTuple(args, "O:set_callback", &python_callback)) {
 		return NULL;
 	}
-	auto callback = [](const char* key, size_t keybytes, const char* value, size_t valuebyte, void* context) -> int {
-		PyObject* args = Py_BuildValue("(s#s#)", key, keybytes, value, valuebyte);
-		PyObject_CallObject((PyObject *) context, args);
-		return 0;
-	};
-	int result = pmemkv_get_all(self->db, callback,  python_callback);
+	int result = pmemkv_get_all(self->db, key_value_callback, python_callback);
+	if (PyErr_Occurred() != NULL)
+		return NULL;
 	if (result != PMEMKV_STATUS_OK) {
 		PyErr_SetObject(PyExc_Exception, PyLong_FromLong(result));
 		return NULL;
@@ -278,12 +444,10 @@ pmemkv_NI_GetAbove(PmemkvObject *self, PyObject* args) {
 	if (!PyArg_ParseTuple(args, "s*O:set_callback", &key, &python_callback)) {
 		return NULL;
 	}
-	auto callback = [](const char* key, size_t keybytes, const char* value, size_t valuebyte, void* context) -> int {
-		PyObject* args = Py_BuildValue("(s#s#)", key, keybytes, value, valuebyte);
-		PyObject_CallObject((PyObject*) context, args);
-		return 0;
-	};
-	int result = pmemkv_get_above(self->db, (const char*) key.buf, key.len, callback, python_callback);
+	int result = pmemkv_get_above(self->db, (const char *)key.buf, key.len,
+				      key_value_callback, python_callback);
+	if (PyErr_Occurred() != NULL)
+		return NULL;
 	if (result != PMEMKV_STATUS_OK) {
 		PyErr_SetObject(PyExc_Exception, PyLong_FromLong(result));
 		return NULL;
@@ -298,12 +462,10 @@ pmemkv_NI_GetBelow(PmemkvObject *self, PyObject* args) {
 	if (!PyArg_ParseTuple(args, "s*O:set_callback", &key, &python_callback)) {
 		return NULL;
 	}
-	auto callback = [](const char* key, size_t keybytes, const char* value, size_t valuebyte, void* context) -> int {
-		PyObject* args = Py_BuildValue("(s#s#)", key, keybytes, value, valuebyte);
-		PyObject_CallObject((PyObject *) context, args);
-		return 0;
-	};
-	int result = pmemkv_get_below(self->db, (const char*) key.buf, key.len, callback, python_callback);
+	int result = pmemkv_get_below(self->db, (const char *)key.buf, key.len,
+				      key_value_callback, python_callback);
+	if (PyErr_Occurred() != NULL)
+		return NULL;
 	if (result != PMEMKV_STATUS_OK) {
 		PyErr_SetObject(PyExc_Exception, PyLong_FromLong(result));
 		return NULL;
@@ -318,12 +480,11 @@ pmemkv_NI_GetBetween(PmemkvObject *self, PyObject* args) {
 	if (!PyArg_ParseTuple(args, "s*s*O:set_callback", &key1, &key2, &python_callback)) {
 		return NULL;
 	}
-	auto callback = [](const char* key, size_t keybytes, const char* value, size_t valuebyte, void* context) -> int {
-		PyObject* args = Py_BuildValue("(s#s#)", key, keybytes, value, valuebyte);
-		PyObject_CallObject((PyObject *) context, args);
-		return 0;
-	};
-	int result = pmemkv_get_between(self->db, (const char*) key1.buf, key1.len, (const char*) key2.buf, key2.len, callback, python_callback);
+	int result = pmemkv_get_between(self->db, (const char *)key1.buf, key1.len,
+					(const char *)key2.buf, key2.len,
+					key_value_callback, python_callback);
+	if (PyErr_Occurred() != NULL)
+		return NULL;
 	if (result != PMEMKV_STATUS_OK) {
 		PyErr_SetObject(PyExc_Exception, PyLong_FromLong(result));
 		return NULL;
@@ -361,8 +522,8 @@ pmemkv_NI_Put(PmemkvObject *self, PyObject* args) {
 	return PyLong_FromLong(result);
 }
 
-static PyObject *
-pmemkv_NI_Get(PmemkvObject *self, PyObject* args) {
+static PyObject *pmemkv_NI_GetString(PmemkvObject *self, PyObject *args)
+{
 	Py_buffer key;
 	if (!PyArg_ParseTuple(args, "s*", &key)) {
 		return NULL;
@@ -379,6 +540,8 @@ pmemkv_NI_Get(PmemkvObject *self, PyObject* args) {
 		c->value.append(v, vb);
 	};
 	int result = pmemkv_get(self->db, (const char*) key.buf, key.len, callback, &cxt);
+	if (PyErr_Occurred() != NULL)
+		return NULL;
 	if (result != PMEMKV_STATUS_OK && result != PMEMKV_STATUS_NOT_FOUND) {
 		PyErr_SetObject(PyExc_Exception, PyLong_FromLong(result));
 		return NULL;
@@ -386,6 +549,24 @@ pmemkv_NI_Get(PmemkvObject *self, PyObject* args) {
 		return Py_BuildValue("s#", cxt.value.data(), cxt.value.size());
 	}
 	Py_RETURN_NONE;
+}
+
+static PyObject *pmemkv_NI_Get(PmemkvObject *self, PyObject *args)
+{
+	Py_buffer key;
+	PyObject *python_callback;
+	if (!PyArg_ParseTuple(args, "s*O:set_callback", &key, &python_callback)) {
+		return NULL;
+	}
+	int result = pmemkv_get(self->db, (const char *)key.buf, key.len, value_callback,
+				python_callback);
+	if (PyErr_Occurred() != NULL)
+		return NULL;
+	if (result != PMEMKV_STATUS_OK) {
+		PyErr_SetString(PyExc_Exception, pmemkv_errormsg());
+		return NULL;
+	}
+	return PyLong_FromLong(result);
 }
 
 static PyObject *
@@ -407,13 +588,14 @@ static PyMethodDef pmemkv_NI_methods[] = {
 	{"start", (PyCFunction)pmemkv_NI_Start, METH_VARARGS, NULL},
 	{"stop", (PyCFunction)pmemkv_NI_Stop, METH_NOARGS, NULL},
 	{"put", (PyCFunction)pmemkv_NI_Put, METH_VARARGS, NULL},
+	{"get_string", (PyCFunction)pmemkv_NI_GetString, METH_VARARGS, NULL},
 	{"get", (PyCFunction)pmemkv_NI_Get, METH_VARARGS, NULL},
 	{"get_keys", (PyCFunction)pmemkv_NI_GetKeys, METH_VARARGS, NULL},
 	{"get_keys_above", (PyCFunction)pmemkv_NI_GetKeysAbove, METH_VARARGS, NULL},
 	{"get_keys_below", (PyCFunction)pmemkv_NI_GetKeysBelow, METH_VARARGS, NULL},
 	{"get_keys_between", (PyCFunction)pmemkv_NI_GetKeysBetween, METH_VARARGS, NULL},
 	{"count_all", (PyCFunction)pmemkv_NI_CountAll, METH_NOARGS, NULL},
-	{"count_above", (PyCFunction)pmemkv_NI_CountAbove, METH_VARARGS, NULL}, 
+	{"count_above", (PyCFunction)pmemkv_NI_CountAbove, METH_VARARGS, NULL},
 	{"count_below", (PyCFunction)pmemkv_NI_CountBelow, METH_VARARGS, NULL},
 	{"count_between", (PyCFunction)pmemkv_NI_CountBetween, METH_VARARGS, NULL},
 	{"get_all", (PyCFunction)pmemkv_NI_GetAll, METH_VARARGS, NULL},
@@ -422,20 +604,19 @@ static PyMethodDef pmemkv_NI_methods[] = {
 	{"get_between", (PyCFunction)pmemkv_NI_GetBetween, METH_VARARGS, NULL},
 	{"exists", (PyCFunction)pmemkv_NI_Exists, METH_VARARGS, NULL},
 	{"remove", (PyCFunction)pmemkv_NI_Remove, METH_VARARGS, NULL},
-	{NULL, NULL, 0, NULL}
-};
+	{NULL, NULL, 0, NULL}};
 
 static PyTypeObject PmemkvType = {
 	PyVarObject_HEAD_INIT(NULL, 0)
 	.tp_name = "pmemkv.pmemkv_NI",
 	.tp_basicsize = sizeof(PmemkvObject),
 	.tp_itemsize = 0,
-	.tp_dealloc = (destructor) Pmemkv_dealloc,
+	.tp_dealloc = (destructor)Pmemkv_dealloc,
 	.tp_print = 0,
 	.tp_getattr = 0,
 	.tp_setattr = 0,
-	.tp_as_async =  0,
-	.tp_repr =  0,
+	.tp_as_async = 0,
+	.tp_repr = 0,
 	.tp_as_number = 0,
 	.tp_as_sequence = 0,
 	.tp_as_mapping = 0,
@@ -444,8 +625,9 @@ static PyTypeObject PmemkvType = {
 	.tp_str = 0,
 	.tp_getattro = 0,
 	.tp_setattro = 0,
-	.tp_flags =  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
-	.tp_doc =  "Pmemkv binding", /* tp_doc */
+	.tp_as_buffer = 0,
+	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+	.tp_doc = "Pmemkv binding", /* tp_doc */
 	.tp_traverse = 0,
 	.tp_clear = 0,
 	.tp_richcompare = 0,
@@ -453,14 +635,14 @@ static PyTypeObject PmemkvType = {
 	.tp_iter = 0,
 	.tp_iternext = 0,
 	.tp_methods = pmemkv_NI_methods,
-	.tp_members =  pmemkv_NI_members,
+	.tp_members = pmemkv_NI_members,
 	.tp_getset = 0,
 	.tp_base = 0,
 	.tp_dict = NULL,
 	.tp_descr_get = 0,
 	.tp_descr_set = 0,
 	.tp_dictoffset = 0,
-	.tp_init = (initproc) Pmemkv_init,
+	.tp_init = (initproc)Pmemkv_init,
 	.tp_alloc = 0,
 	.tp_new = Pmemkv_new,
 	.tp_free = 0,
