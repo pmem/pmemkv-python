0c0799a2d4fe335aceca286626b30a6c99321273
Pawel Karczewski <pawel.karczewski@intel.com>
2020-01-10 13:26:38 +0100

    Add brief documentation in docstring
    To view documentation:
    	import pmemkv
    	help(pmemkv)
    	help(pmemkv.pmemkv)

--- pmemkv/pmemkv.py
+++ pmemkv/pmemkv.py
@@ -1,41 +1,40 @@
-'''
- * Copyright 2019, Intel Corporation
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in
- *       the documentation and/or other materials provided with the
- *       distribution.
- *
- *     * Neither the name of the copyright holder nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-'''
+#  Copyright 2019, Intel Corporation
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in
+#        the documentation and/or other materials provided with the
+#        distribution.
+#
+#      * Neither the name of the copyright holder nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+""" Python bindings for pmemkv """
 
 import _pmemkv
 
 class Database():
 
-    # Starts the given engine.
-    # Takes engine name and configuration from the end user.
+
     def __init__(self, engine, config):
         self.db = _pmemkv.pmemkv_NI()
         self.db.start(engine, config)
@@ -59,91 +58,101 @@ class Database():
             raise KeyError(key)
         self.remove(key)
 
-    # Stops the running engine.
     def stop(self):
+        """ Stops the running engine."""
         self.db.stop()
 
-    # Puts the key/value pair into pmemkv datastore.
-    # Takes key & value from the end user.
     def put(self, key, value):
+        """ Inserts the key/value pair into pmemkv datastore. """
         return self.db.put(key, value)
 
-    # Fetches all the keys from pmemkv datastore.
-    # Takes callback from the end user and sends the resulted keys through callback.
     def get_keys(self, func):
+        """ Executes callback function for every key stored in
+        pmemkv datastore.
+        """
         return self.db.get_keys(func)
 
-    # Fetches all the keys from the begining of the pmemkv datastore till key matched.
-    # Takes key and callback from the end user and sends the resulted keys through callback.
     def get_keys_above(self, key, func):
+        """ Executes callback function for every key stored in
+        pmemkv datastore, whose keys are greater than the given key.
+        """
         return self.db.get_keys_above(key, func)
 
-    # Fetches all the keys from the key matched in pmemkv datastore till end.
-    # Takes key and callback from the end user and sends the resulted keys through callback.
     def get_keys_below(self, key, func):
+        """ Executes callback function for every key stored in
+        pmemkv datastore, whose keys are lower than the given key.
+        """
         return self.db.get_keys_below(key, func)
 
-    # Fetches all the keys present, between key1 and key2 from pmemkv datastore.
-    # Takes key1, key2 and callback from the end user and sends the resulted keys through callback.
     def get_keys_between(self, key1, key2, func):
+        """ Executes callback function for every key stored in pmemkv
+        datastore, whose keys are greater than the key1 and less than the key2.
+        """
         return self.db.get_keys_between(key1, key2, func)
 
-    # Counts the total number of keys in the pmemkv datastore.
-    # Returns total number of keys.
     def count_all(self):
+        """ Returns number of currently stored elements in pmemkv datastore."""
         return self.db.count_all()
 
-    # Counts the total number of keys from the begining of the pmemkv datastore till key matched.
-    # Takes key from the end user, returns number of resulted keys.
+
     def count_above(self, key):
+        """ Returns number of currently stored elements in pmemkv datastore,
+        whose keys are greater than the given key
+        """
         return self.db.count_above(key)
 
-    # Counts the total number of keys from the key matched in the pmemkv datastore till end.
-    # Takes key from the end user, returns number of resulted keys.
+
     def count_below(self, key):
+        """ Returns number of currently stored elements in pmemkv datastore,
+        whose keys are less than the given key.
+        """
         return self.db.count_below(key)
 
-    # Counts the total number of keys present, between key1 and key2 from pmemkv datastore.
-    # Takes key1 and key2 from the end user, returns number of resulted keys.
+
     def count_between(self, key1, key2):
+        """ Returns number of currently stored elements in pmemkv datastore,
+        whose keys are greater than the key1 and less than the key2
+        """
         return self.db.count_between(key1, key2)
 
-    # Fetches all the key/value pairs from pmemkv datastore.
-    # Takes callback from the end user and sends the resulted key/value pairs through callback.
     def get_all(self, func):
+        """ Executes callback function for every record stored in pmemkv
+        datastore.
+        """
         return self.db.get_all(func)
 
-    # Fetches all the key/value pairs from the begining of the pmemkv datastore till key matched.
-    # Takes key and callback from the end user and sends the resulted key/value pairs through callback.
     def get_above(self, key, func):
+        """ Executes callback function for every key/value pair stored in
+        pmemkv datastore, whose keys are greater than the given key.
+        """
         return self.db.get_above(key, func)
 
 
-    # Fetches all the key/value pairs from the key matched in the pmemkv datastore till end.
-    # Takes key and callback from the end user and sends the resulted key/value pairs through callback.
     def get_below(self, key, func):
+        """ Executes callback function for every key/value pair stored in
+        pmemkv datastore, whose keys are lower than the given key.
+        """
         return self.db.get_below(key, func)
 
-    # Fetches all the key/value pairs present, between key1 and key2 from pmemkv datastore.
-    # Takes key1, key2 and callback from the end user and sends the resulted key/value pairs through callback.
     def get_between(self, key1, key2, func):
+        """ Executes callback function for every key/value pair stored in
+        pmemkv datastore, whose keys are greater than the key1 and less
+        than the key2.
+        """
         return self.db.get_between(key1, key2, func)
 
-    # Verifies the key presence in pmemkv datastore.
-    # Takes key from the end user and returns the key presence.
     def exists(self, key):
+        """ Verifies the key presence in pmemkv datastore."""
         return self.db.exists(key)
 
-    # Gets the value for the given key from pmemkv datastore.
-    # Takes key from the end user and returns the value.
     def get(self, key, func):
+        """ Executes callback function for value for given key. """
         return self.db.get(key, func)
 
-    # Gets the value for the given key from pmemkv datastore.
-    # Takes key and encoding algorithm from the end user and returns the encoded value.
     def get_string(self, key):
+        """ Gets copy (as a string) of value for given key """
         return self.db.get_string(key)
 
-    # Takes key from the end user and returns the key removal status.
     def remove(self, key):
+        """ Removes key/value pair from pmemkv datastore for given key."""
         return self.db.remove(key)
