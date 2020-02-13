b4f5fab6f41332fdd356fc19747219ec506e14f5
Pawel Karczewski <pawel.karczewski@intel.com>
2020-02-13 18:13:28 +0100

    Configure setup for PyPi
    Follow python packing guide in  setup implementation.
    
    https://packaging.python.org/tutorials/packaging-projects/
    https://github.com/pypa/sampleproject/blob/master/setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-'''
- * Copyright 2019, Intel Corporation
+"""
+ * Copyright 2019-2020, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,13 +28,40 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-'''
+"""
 import setuptools
+from os import path
 
-link_modules = setuptools.Extension('_pmemkv', ['pmemkv/kvengine.cc'], libraries = ['pmemkv', 'pmemkv_json_config'])
+project_dir = path.abspath(path.dirname(__file__))
+with open(path.join(project_dir, "README.md"), encoding="utf-8") as f:
+    readme = f.read()
 
-setuptools.setup(name= 'pmemkv',
-    version = '0.9',
-    description = 'Python bindings for PMEMKV Engine',
-    packages = setuptools.find_packages(),
-    ext_modules = [link_modules])
+link_modules = setuptools.Extension(
+    "_pmemkv", ["pmemkv/kvengine.cc"], libraries=["pmemkv", "pmemkv_json_config"]
+)
+
+setuptools.setup(
+    name="pmemkv",
+    version="1.0",
+    description="Python bindings for PMEMKV library",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pmem/pmemkv-python",
+    author="Pmemkv development team",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
+    python_requires=">=3.6",
+    project_urls={
+        "Source Code": "https://github.com/pmem/pmemkv-python",
+        "Bug Reports": "https://github.com/pmem/pmemkv-python/issues",
+        "Pmemkv": "https://github.com/pmem/pmemkv",
+    },
+    ext_modules=[link_modules],
+)
